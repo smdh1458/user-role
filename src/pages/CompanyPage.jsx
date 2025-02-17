4db3af4a4801f900aa5d8aa9411610e0d30d2823
smdh1458 <smdh1458@hanmail.net>
2025-02-17 12:48:52 +0900

    user-role company

---
+++ src/pages/CompanyPage.jsx
@@ -0,0 +1,12 @@
+
+
+const CompanyPage = ({user}) => {
+    return(
+        <div>
+            <h1>업체페이지</h1>
+            <p>환영합니다.{user.userName}님</p>
+        </div>
+    )
+}
+
+export default CompanyPage;
